9281737f65936f1858812d921a2974fe315bfc82
장진우 <arzz@arzz.com>
2016-09-03 12:37:48 +0900

    에러페이지 템플릿 추가

---
+++ includes/error.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 이 파일은 iModule 의 일부입니다. (https://www.imodule.kr)
+ *
+ * 사이트를 구성할 수 없는 치명적인 에러가 발생할 경우 사이트 레이아웃 구성을 모두 취소하고 이 에러페이지를 출력합니다.
+ * 사이트 레이아웃을 구성할 수 있는 에러일 경우 사이트 템플릿의 에러메세지 템플릿이나 모듈 에러메세지 이용하여 에러메세지를 출력하게 됩니다.
+ * @file /classes/iModule.class.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0.160831
+ * @see /classes/iModule.class.php -> printError()
+ */
+ 
+if ($IM->language == 'ko') {
+	$IM->loadWebFont('NanumGothic',true);
+	$IM->loadWebFont('OpenSans');
+} else {
+	$IM->loadWebFont('OpenSans',true);
+}
+?>
+<main>
+	<h1><i class="fa fa-exclamation-triangle"></i> ERROR!</h1>
+	
+	<h2><?php echo $title; ?></h2>
+	<?php if ($message) { ?><p><?php echo $message; ?></p><?php } ?>
+	
+	<a href="<?php echo $IM->getUrl(false); ?>"><?php echo $IM->getLanguage('error/backToMain'); ?></a>
+</main>
