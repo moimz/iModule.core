ccd4275cfb75782c69d368a8dd3dba99a35f6f92
JRevirthu'um <revirthuum@gmail.com>
2016-11-10 17:51:33 +0900

    Windows 서버상의 경로이슈 처리

--- admin/index.php
+++ admin/index.php
@@ -16,7 +16,7 @@
  *
  * @see /configs/init.config.php
  */
-REQUIRE_ONCE str_replace(DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'index.php','',$_SERVER['SCRIPT_FILENAME']).'/configs/init.config.php';
+REQUIRE_ONCE str_replace('/admin','',dirname($_SERVER['SCRIPT_FILENAME'])).'/configs/init.config.php';
 
 $IM = new iModule();
 $IM->getModule('admin')->doLayout();
