a7509ec23384adc1f3862b4d8e829fb9789d39cd
장진우 <arzz@arzz.com>
2017-08-25 23:06:57 +0900

    Fixed notice error

--- styles/font.css.php
+++ styles/font.css.php
@@ -1,11 +1,13 @@
 <?php
-header('Content-Type:text/css');
+header("Content-Type:text/css");
 
-$language = $_GET['language'];
-$font = explode(',',$_GET['font']);
+$language = isset($_GET['language']) == true ? $_GET['language'] : 'ko';
+$font = isset($_GET['font']) == true ? explode(',',$_GET['font']) : array();
 $default = isset($_GET['default']) == true ? $_GET['default'] : null;
 
-for ($i=0, $loop=count($font);$i<$loop;$i++) echo file_get_contents('./fonts/'.$font[$i].'.css');
+for ($i=0, $loop=count($font);$i<$loop;$i++) {
+	if (is_file('./fonts/'.$font[$i].'.css') == true) echo file_get_contents('./fonts/'.$font[$i].'.css');
+}
 
 if ($default != null) $fontFamily = $default.', ';
 else $fontFamily = '';
