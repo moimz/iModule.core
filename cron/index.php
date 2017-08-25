25b49de0936cedd3f97755ce11c4ec0807fc13ec
장진우 <arzz@arzz.com>
2017-08-25 23:10:38 +0900

    Add crontab daemons

---
+++ cron/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 이 파일은 iModule 의 일부입니다. (https://www.imodule.kr)
+ *
+ * iModule 크론작업을 실행한다.
+ * 
+ * @file /cron/index.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0.161110
+ */
+REQUIRE_ONCE str_replace('/cron','',dirname(__FILE__)).'/classes/Cron.class.php';
+
+$hosts = array_slice($argv,1);
+$cron = new Cron($hosts);
+$cron->run();
+exit;
+?>
