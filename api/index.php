3ec6284cd1f6a357a345a377727155be2111eb1f
arzzcom <arzz@arzz.com>
2017-11-23 07:30:06 +0900

    API 방식 수정

--- api/index.php
+++ api/index.php
@@ -7,7 +7,8 @@
  * @file index.php
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
- * @version 3.0.0.161110
+ * @version 3.0.0
+ * @modified 2017. 11. 22.
  */
 
 header('Cache-Control:no-store, no-cache, must-revalidate, max-age=0');
@@ -31,17 +32,19 @@ $results = new stdClass();
 $_module = Request('_module');
 if ($_module != null && $IM->Module->isInstalled($_module) == true) {
 	$mModule = $IM->getModule($_module);
-	$method = $_SERVER['REQUEST_METHOD'];
+	$protocol = strtolower($_SERVER['REQUEST_METHOD']);
 	
 	$_api = Request('_api');
-	$idx = Request('idx');
-	if ($method == 'POST' && method_exists($mModule,'postApi') == true) {
-		$params = $_POST;
-		$data = $mModule->postApi($_api,$idx,$params);
-	} elseif ($method == 'GET' && method_exists($mModule,'getApi') == true) {
-		$params = $_GET;
-		unset($params['module'],$params['api'],$params['idx']);
-		$data = $mModule->getApi($_api,$idx,$params);
+	$_idx = Request('_idx');
+	
+	if (method_exists($mModule,'getApi') == true) {
+		$params = $_REQUEST;
+		unset($_REQUEST['_module']);
+		unset($_REQUEST['_api']);
+		unset($_REQUEST['_idx']);
+		
+		$params = count($params) == 0 ? null : (object)$params;
+		$data = $mModule->getApi($protocol,$_api,$_idx,$params);
 	} else {
 		$data = null;
 	}
