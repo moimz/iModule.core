17e946f09a748f358e0728707aa798bdfb4364e6
arzzcom <arzz@arzz.com>
2019-05-16 17:07:06 +0900

    HTML DOM 파서 클래스 추가

---
+++ classes/Html.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 이 파일은 iModule 의 일부입니다. (https://www.imodules.io)
+ *
+ * Html DOM을 파싱한다.
+ * 
+ * @file /classes/Html.class.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0
+ * @modified 2019. 5. 16.
+ */
+REQUIRE_ONCE __DIR__.'/Html/simple_html_dom.php';
+
+class Html extends simple_html_dom {
+}
+?>
