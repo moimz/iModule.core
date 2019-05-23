49e94f3b4d202cbeef0c0a9748d4afd15ce1a54d
arzzcom <arzz@arzz.com>
2019-05-23 15:06:59 +0900

    모달창이 다른 스타일에 종속되지 않도록 DOM 구조 변경

--- includes/modal.php
+++ includes/modal.php
@@ -8,25 +8,25 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 3.0.0
- * @modified 2018. 12. 21.
+ * @modified 2019. 5. 23.
  */
 if (defined('__IM__') == false) exit;
 ?>
 <div data-role="modal" data-closable="<?php echo $is_closable == true ? 'TRUE' : 'FALSE'; ?>" data-fullsize="<?php echo $is_fullsize == true ? 'TRUE' : 'FALSE'; ?>" data-width="<?php echo $width; ?>" data-height="<?php echo $height; ?>" data-max-width="<?php echo $max_width; ?>" data-max-height="<?php echo $max_height; ?>">
 	<?php echo $header; ?>
-	<header>
+	<div data-role="header">
 		<h1><?php echo $title; ?></h1>
 		<button type="button" data-action="close"><i class="mi mi-close"></i></button>
-	</header>
+	</div>
 	
-	<main>
+	<div data-role="context">
 		<?php echo $content; ?>
-	</main>
+	</div>
 	
-	<footer>
+	<div data-role="footer">
 		<?php foreach ($buttons as $button) { ?>
 		<div><button <?php echo $button->type == 'submit' ? 'type="submit"' : 'type="button" data-action="'.$button->type.'"'; ?><?php echo isset($button->class) == true && $button->class ? ' class="'.$button->class.'"' : ''; ?>><?php echo $button->text; ?></button></div>
 		<?php } ?>
-	</footer>
+	</div>
 	<?php echo $footer; ?>
 </div>
